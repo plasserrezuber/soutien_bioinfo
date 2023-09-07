f6fc1bc8321718c4f30954d8401582562bb7f2c8
Pauline Lasserre-Zuber <pauline.lasserre-zuber@inra.fr>
2023-09-07 16:16:55 +0200

    smk pipe improvement

--- BOUGUENNEC/bin/CLARI-TE_smk/bin/embl_to_gff3.sh
+++ BOUGUENNEC/bin/CLARI-TE_smk/bin/embl_to_gff3.sh
@@ -4,20 +4,22 @@
 ## NB: script embl2gff.generic.pl gives an ID per TE per chrom (not per chunk) from zero if a list of ordered files for all chunks is given for each chrom
 ## That is why the $embl_files variable is created before
 
-embl_files=$(\ls -1 ../results/${1}/${1}*.embl |sort -t ':' -k2,2n |tr -s '\n' ' ')
-## !!!!! revoir option "renan"
-bin/embl2gff.generic.pl -renan -note -l 10 $embl_files > ../results/${1}_clariTE.gff
+embl_files=$(\ls -1 results/${1}/${1}*.embl |sort -t ':' -k2,2n |tr -s '\n' ' ')
 
-###### gff to gff3: new coordinates calculation with gawk command (from chunks relative to chrom relative)
-grep -v $'\t''region' ../results/${1}_clariTE.gff |sed 's/embl2gff.generic.pl-2.1/clariTE/' \
+#option -note: Convert all tags/values to a single tag Note="tag:xxx; tag:xxx; tag:xxx"
+#option -l <interger>: Length threshold to skip small features [default: 1 bp]
+bin/embl2gff.generic.pl -RMclariTE -featurePrefix $3 -source clariTE -note -l 10 $embl_files > results/${1}_clariTE.gff
+
+###### gff to gff3: new coordinates calculation with gawk commands "match" + "substr" (from chunks relative to chrom relative)
+grep -v $'\t''region' results/${1}_clariTE.gff \
 |gawk -v FS='\t' -v OFS='\t' '/^chr/ {match($1, /:[0-9]+-/); $4=$4+substr($0, RSTART+1, RLENGTH-2); $5=$5+substr($0, RSTART+1, RLENGTH-2); print}' \
-|gt gff3 -sort -tidy -retainids 1> ../results/${1}_clariTE_tmp.gff3  2> ../results/${1}_clariTE_tmp_gt.log
+|gt gff3 -sort -tidy -retainids 1> results/${1}_clariTE_tmp.gff3  2> logs/${1}_clariTE_tmp_gt.log
 
 ### variable $endchrom to give the right sequence-region coordinate in gff3
 endchrom=$(grep ${1} ${2}.fai |cut -f2)
 
 # grep -v command keeps only one "sequence-region" line starting with zero coordinate
 # gawk and sed commands to format in a more friendly way
-grep -v -P '##sequence-region *'$1':[1-9]' ../results/${1}_clariTE_tmp.gff3 \
-|gawk -v LG=$1 -v end=$endchrom 'BEGIN{FS='\t';OFS='\t'} { if ($0~'##sequence-region') $0='##sequence-region\t'LG'\t1\t'end; print }' \
-|sed -E 's/'$1':[0-9]*-[0-9]*/'$1'/' |sed -E 's/Compo:.* (Family)/\1/' | sed -E 's/Post:.* (Status)/\1/' > ../results/${1}_clariTE.gff3
+grep -v -P '##sequence-region *'$1':[1-9]' results/${1}_clariTE_tmp.gff3 \
+|gawk -v LG=$1 -v end=$endchrom 'BEGIN{FS="\t";OFS="\t"} { if ($0~"##sequence-region") $0="##sequence-region\t"LG"\t1\t"end; print }' \
+|sed -E 's/'$1':[0-9]*-[0-9]*/'$1'/' |sed -E 's/Compo:.* (Family)/\1/' | sed -E 's/Post:.* (Status)/\1/' > results/${1}_clariTE.gff3
