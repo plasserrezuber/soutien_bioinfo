70a76eaecccfa9675e1645a6075307fe2b36b764
Pauline Lasserre-Zuber <pauline.lasserre-zuber@inra.fr>
2023-10-13 14:32:30 +0200

    successful singularity def and embl_to_gff3 correction

--- BOUGUENNEC/bin/CLARI-TE_smk/bin/embl_to_gff3.sh
+++ BOUGUENNEC/bin/CLARI-TE_smk/bin/embl_to_gff3.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ###### embl2gff: takes all chunks embl and produces one merged .gff per chrom
-## NB: script embl2gff.generic.pl gives an ID per TE per chrom (not per chunk) from zero if a list of ordered files for all chunks is given for each chrom
+## NB: script embl2gff.pl gives an ID per TE per chrom (not per chunk) from zero if a list of ordered files for all chunks is given for each chrom
 ## That is why the $embl_files variable is created before
 
 embl_files=$(cat results/chrom/${1}_chunks_embl_list.txt)
 
 #option -note: Convert all tags/values to a single tag Note="tag:xxx; tag:xxx; tag:xxx"
 #option -l <interger>: Length threshold to skip small features [default: 1 bp]
-bin/embl2gff.generic.pl -RMclariTE -featurePrefix $2 -source clariTE -note -l 10 $embl_files > results/${1}_clariTE.gff
+bin/embl2gff.pl -RMclariTE -featurePrefix $2 -source clariTE -note -l 10 $embl_files > results/${1}_clariTE.gff
 
 ###### gff to gff3: new coordinates calculation with gawk commands "match" + "substr" (from chunks relative to chrom relative)
 grep -v $'\t''region' results/${1}_clariTE.gff \
