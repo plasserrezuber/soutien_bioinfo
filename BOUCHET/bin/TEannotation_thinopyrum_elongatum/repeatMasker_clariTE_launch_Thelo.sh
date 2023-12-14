974c5349f1ddc8992fcc38a24e499baf9b24b791
Pauline Lasserre-Zuber <pauline.lasserre-zuber@inra.fr>
2023-12-14 17:04:48 +0100

    update 2 TEannot Thelo

--- BOUCHET/bin/TEannotation_thinopyrum_elongatum/repeatMasker_clariTE_launch_Thelo.sh
+++ BOUCHET/bin/TEannotation_thinopyrum_elongatum/repeatMasker_clariTE_launch_Thelo.sh
@@ -35,24 +35,24 @@ else
 fi
 
 samtools faidx ${chrom}.fasta
-# bedtools makewindows -g ${chrom}.fasta.fai -w 10000000 > ${chrom}.windows.bed
+bedtools makewindows -g ${chrom}.fasta.fai -w 10000000 > ${chrom}.windows.bed
 
-# bedtools getfasta -bed ${chrom}.windows.bed -fi ${chrom}.fasta > ${chrom}.windows.fasta
-# fastaexplode -f ${chrom}.windows.fasta -d $OUTPUT/${chrom}
+bedtools getfasta -bed ${chrom}.windows.bed -fi ${chrom}.fasta > ${chrom}.windows.fasta
+fastaexplode -f ${chrom}.windows.fasta -d $OUTPUT/${chrom}
 
 files=($(find ${chrom}*.fa))
 k=$((${#files[@]}-1))  #recupere le nb d'item dans la liste 'files'
 
 
 # ###### RepeatMasker
-# RMjid=`sbatch --wait --parsable -J ${chr}_RM --array=0-${k}%20 /home/palasser/projects/soutien_bioinfo/BOUCHET/bin/repeatMasker_Thelo.sh ${chrom}`
+RMjid=`sbatch --wait --parsable -J ${chr}_RM --array=0-${k}%20 /home/palasser/projects/soutien_bioinfo/BOUCHET/bin/repeatMasker_Thelo.sh ${chrom}`
 
-# echo "$RMjid : RepeatMasker on ${chrom}"
+echo "$RMjid : RepeatMasker on ${chrom}"
 
-# if [ $(find ${chrom}:*.fa.out.xm |wc -l) = $(($k+1)) ];
-#     then echo ${chrom}": all RepeatMasker output files there";
-#     else echo ${chrom}": missing "$(($(find ${chrom}:*.fa |wc -l) - $(find ${chrom}:*.fa.out.xm |wc -l) ))" RepeatMasker output files";
-# fi
+if [ $(find ${chrom}:*.fa.out.xm |wc -l) = $(($k+1)) ];
+    then echo ${chrom}": all RepeatMasker output files there";
+    else echo ${chrom}": missing "$(($(find ${chrom}:*.fa |wc -l) - $(find ${chrom}:*.fa.out.xm |wc -l) ))" RepeatMasker output files";
+fi
 
 # ## rescue example:
 # # sbatch -J 6A_RMrescue -p normal -c 16 --mem=8G --wrap="module load gcc/4.8.4 triannotTools/1.2 gdecTools/1.1 RepeatMasker; cd /home/palasser/projects/soutien_bioinfo/BOUCHET/results/annotTE_Thelo/chr6A; RepeatMasker -e crossmatch -lib /storage/groups/gdec/triannot/tools/ClariTE/clariTE_1.0/databank/CLARIwheat.fasta -xsmall -nolow -xm -pa 16 -q /home/palasser/projects/soutien_bioinfo/BOUCHET/results/annotTE_Thelo/chr6A/chr6A:80000000-90000000.fa"
@@ -61,9 +61,7 @@ k=$((${#files[@]}-1))  #recupere le nb d'item dans la liste 'files'
 
 ###### clariTE
 ## option `--wait` pour que le script principal attende la fin du job array clariTE avant cmd suivante
-# --dependency=afterok:$RMjid 
-CLARITEjid=`sbatch --wait --parsable -J ${chr}_clariTE --array=0-$k%20 /home/palasser/projects/soutien_bioinfo/BOUCHET/bin/clariTE_Thelo.sh ${chrom}`
-# CLARITEjid=`sbatch --wait --parsable -J ${chr}_clariTE --array=0-$k%20 /home/palasser/projects/soutien_bioinfo/BOUCHET/bin/clariTE_Thelo.sh ${chrom}`
+CLARITEjid=`sbatch --dependency=afterok:$RMjid --wait --parsable -J ${chr}_clariTE --array=0-$k%20 /home/palasser/projects/soutien_bioinfo/BOUCHET/bin/clariTE_Thelo.sh ${chrom}`
 
 echo "$CLARITEjid : clariTE on ${chrom}"
 
@@ -76,7 +74,13 @@ fi
 ## le script embl2gff.generic.pl numerote les TE par chrom (et non par chunk) en repartant de zero pour chaque chrom a condition
 ## de fournir la liste ordonnee des fichiers pour l'ensemble des chunks par chromosome
 embl_files=$(\ls -1 ${chrom}*.embl |sort -t ':' -k2,2n |tr -s '\n' ' ')
-/storage/groups/gdec/bin/scripts/embl2gff.pl -RMclariTE -featurePrefix Thelo_ -note -l 10 $embl_files > $OUTPUT/Thelo_RefSeq_${chr}_clariTE.gff
+
+if [ $chrom = "chrUn" ];
+    then 
+    /storage/groups/gdec/bin/scripts/embl2gff.pl -RMclariTE -featurePrefix Thelo_chrUn -note -l 10 $embl_files > $OUTPUT/Thelo_RefSeq_${chr}_clariTE.gff
+else
+    /storage/groups/gdec/bin/scripts/embl2gff.pl -RMclariTE -featurePrefix Thelo_ -note -l 10 $embl_files > $OUTPUT/Thelo_RefSeq_${chr}_clariTE.gff
+fi
 
 ###### gff to gff3: recalcul des coordonnees avec gawk (transfo de relatives aux chunks vers relatives au chrom) + mise en forme gff3 par GenomeTools sort
 grep -v $'\t''region' $OUTPUT/Thelo_RefSeq_${chr}_clariTE.gff |sed 's/embl2gff.pl-2.2/clariTE/' \
@@ -91,7 +95,7 @@ if [ $chrom = "chrUn" ];
     #le premier grep garde la premiere "##sequence-region" et les annotation de TEs
     grep -P '##sequence-region *'$chrom':JAAAXO010000008|^chr|###' $OUTPUT/Thelo_RefSeq_${chr}_clariTE.gff3 \
     |gawk -v LG=$chrom -v end=$endchrom 'BEGIN{FS="\t";OFS="\t"} { if ($0~"##sequence-region") $0="##sequence-region\t"LG"\t1\t"end; print }' \
-    |sed -E 's/'$chrom':[0-9]*-[0-9]*/'$chrom'/' |sed -E 's/Compo:.* (Family)/\1/' | sed -E 's/Post:.* (Status)/\1/' > $OUTPUT/Thelo_RefSeq_${chr}_clariTE_friendly.gff3
+    |sed -E 's/'$chrom':JAAAXO.*embl.1/'$chrom'/' |sed -E 's/Compo:.* (Family)/\1/' | sed -E 's/Post:.* (Status)/\1/' > $OUTPUT/Thelo_RefSeq_${chr}_clariTE_friendly.gff3
 else
     endchrom=$(grep $accessNum ${GENOME}.fai |cut -f2)
     grep -v -P '##sequence-region *'$chrom':[1-9]' $OUTPUT/Thelo_RefSeq_${chr}_clariTE.gff3 \
